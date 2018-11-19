e5b87c62c2a0fa4c708cc0835cfbc73f96ebf4b5
Torsten Holva <torstenholva@users.noreply.github.com>
2018-11-19 20:17:16 +0200

    Command line example: pwd > /home/ec2-user/GIT/github/SimpleHTR.git/src python3 main.py --wordbeamsearch -fi 02_segmented/6.png/ -fo 6.png/
    > Files in directory: ../../02_segmented/6.png/: 0.png 1.png 2.png 3.png summary.png
    >
    > Validation character error rate of saved model: 15.575379%
    > WARNING:tensorflow:From /home/ec2-user/GIT/github/SimpleHTR.git/src/Model.py:113: calling > softmax (from tensorflow.python.ops.nn_ops) with dim is deprecated and will be removed in > a future version.
    > Instructions for updating:
    > dim is deprecated, use axis instead
    > Python: 3.5.5 (default, Apr 25 2018, 23:51:32)
    > [GCC 4.8.5 20150623 (Red Hat 4.8.5-11)]
    > Tensorflow: 1.11.0
    > 2018-11-19 18:06:24.611823: I tensorflow/core/platform/cpu_feature_guard.cc:141] Your CPU > supports instructions that this TensorFlow binary was not compiled to use: AVX2 FMA
    > Init with stored values from ../model/snapshot-32
    > ../../02_segmented/6.png/0.png
    > ../../02_segmented/6.png/0.png
    > Recognized:  "fall" => written to ../../03_out/6.png/recognized.txt
    > ../../02_segmented/6.png/1.png
    > ../../02_segmented/6.png/1.png
    > Recognized:  "nourishment" => written to ../../03_out/6.png/recognized.txt
    > ../../02_segmented/6.png/2.png
    > ../../02_segmented/6.png/2.png
    > Recognized:  "items" => written to ../../03_out/6.png/recognized.txt
    > ../../02_segmented/6.png/3.png
    > ../../02_segmented/6.png/3.png
    > Recognized:  "pharmaceutical" => written to ../../03_out/6.png/recognized.txt
    > Dismissing summary.png

--- src/main.py
+++ src/main.py
@@ -9,153 +9,174 @@ from SamplePreprocessor import preprocess
 
 
 class FilePaths:
-	"filenames and paths to data"
-	fnCharList = '../model/charList.txt'
-	fnAccuracy = '../model/accuracy.txt'
-	fnTrain = '../data/'
-	fnInfer = '../data/test.png'
-	fnInPath = '../../02_deslanted/'
-	fnOutPath = '../../03_out/'
-	fnCorpus = '../data/corpus.txt'
+    "filenames and paths to data"
+    fnCharList = '../model/charList.txt'
+    fnAccuracy = '../model/accuracy.txt'
+    fnTrain = '../data/'
+    fnInfer = '../data/test.png'
+    fnInPath = '../../'
+    fnOutPath = '../../03_out/'
+    fnCorpus = '../data/corpus.txt'
+    fnRecognizedTxt = 'recognized.txt'
 
 
 def train(model, loader):
-	"train NN"
-	epoch = 0 # number of training epochs since start
-	bestCharErrorRate = float('inf') # best valdiation character error rate
-	noImprovementSince = 0 # number of epochs no improvement of character error rate occured
-	earlyStopping = 5 # stop training after this number of epochs without improvement
-	while True:
-		epoch += 1
-		print('Epoch:', epoch)
+    "train NN"
+    epoch = 0 # number of training epochs since start
+    bestCharErrorRate = float('inf') # best valdiation character error rate
+    noImprovementSince = 0 # number of epochs no improvement of character error rate occured
+    earlyStopping = 5 # stop training after this number of epochs without improvement
+    while True:
+        epoch += 1
+        print('Epoch:', epoch)
 
-		# train
-		print('Train NN')
-		loader.trainSet()
-		while loader.hasNext():
-			iterInfo = loader.getIteratorInfo()
-			batch = loader.getNext()
-			loss = model.trainBatch(batch)
-			print('Batch:', iterInfo[0],'/', iterInfo[1], 'Loss:', loss)
+        # train
+        print('Train NN')
+        loader.trainSet()
+        while loader.hasNext():
+            iterInfo = loader.getIteratorInfo()
+            batch = loader.getNext()
+            loss = model.trainBatch(batch)
+            print('Batch:', iterInfo[0],'/', iterInfo[1], 'Loss:', loss)
 
-		# validate
-		charErrorRate = validate(model, loader)
-		
-		# if best validation accuracy so far, save model parameters
-		if charErrorRate < bestCharErrorRate:
-			print('Character error rate improved, save model')
-			bestCharErrorRate = charErrorRate
-			noImprovementSince = 0
-			model.save()
-			open(FilePaths.fnAccuracy, 'w').write('Validation character error rate of saved model: %f%%' % (charErrorRate*100.0))
-		else:
-			print('Character error rate not improved')
-			noImprovementSince += 1
+        # validate
+        charErrorRate = validate(model, loader)
 
-		# stop training if no more improvement in the last x epochs
-		if noImprovementSince >= earlyStopping:
-			print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
-			break
+        # if best validation accuracy so far, save model parameters
+        if charErrorRate < bestCharErrorRate:
+            print('Character error rate improved, save model')
+            bestCharErrorRate = charErrorRate
+            noImprovementSince = 0
+            model.save()
+            open(FilePaths.fnAccuracy, 'w').write('Validation character error rate of saved model: %f%%' % (charErrorRate*100.0))
+        else:
+            print('Character error rate not improved')
+            noImprovementSince += 1
+
+        # stop training if no more improvement in the last x epochs
+        if noImprovementSince >= earlyStopping:
+            print('No more improvement since %d epochs. Training stopped.' % earlyStopping)
+            break
 
 
 def validate(model, loader):
-	"validate NN"
-	print('Validate NN')
-	loader.validationSet()
-	numCharErr = 0
-	numCharTotal = 0
-	numWordOK = 0
-	numWordTotal = 0
-	while loader.hasNext():
-		iterInfo = loader.getIteratorInfo()
-		print('Batch:', iterInfo[0],'/', iterInfo[1])
-		batch = loader.getNext()
-		recognized = model.inferBatch(batch)
-		
-		print('Ground truth -> Recognized')	
-		for i in range(len(recognized)):
-			numWordOK += 1 if batch.gtTexts[i] == recognized[i] else 0
-			numWordTotal += 1
-			dist = editdistance.eval(recognized[i], batch.gtTexts[i])
-			numCharErr += dist
-			numCharTotal += len(batch.gtTexts[i])
-			print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
-	
-	# print validation result
-	charErrorRate = numCharErr / numCharTotal
-	wordAccuracy = numWordOK / numWordTotal
-	print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
-	return charErrorRate
+    "validate NN"
+    print('Validate NN')
+    loader.validationSet()
+    numCharErr = 0
+    numCharTotal = 0
+    numWordOK = 0
+    numWordTotal = 0
+    while loader.hasNext():
+        iterInfo = loader.getIteratorInfo()
+        print('Batch:', iterInfo[0],'/', iterInfo[1])
+        batch = loader.getNext()
+        recognized = model.inferBatch(batch)
+
+        print('Ground truth -> Recognized')
+        for i in range(len(recognized)):
+            numWordOK += 1 if batch.gtTexts[i] == recognized[i] else 0
+            numWordTotal += 1
+            dist = editdistance.eval(recognized[i], batch.gtTexts[i])
+            numCharErr += dist
+            numCharTotal += len(batch.gtTexts[i])
+            print('[OK]' if dist==0 else '[ERR:%d]' % dist,'"' + batch.gtTexts[i] + '"', '->', '"' + recognized[i] + '"')
+
+    # print validation result
+    charErrorRate = numCharErr / numCharTotal
+    wordAccuracy = numWordOK / numWordTotal
+    print('Character error rate: %f%%. Word accuracy: %f%%.' % (charErrorRate*100.0, wordAccuracy*100.0))
+    return charErrorRate
 
 
-def infer(model, fnImg):
-	"recognize text in image provided by file path"
-	img = preprocess(cv2.imread(fnImg, cv2.IMREAD_GRAYSCALE), Model.imgSize)
-	batch = Batch(None, [img] * Model.batchSize) # fill all batch elements with same input image
-	recognized = model.inferBatch(batch) # recognize text
-	print('Recognized:', '"' + recognized[0] + '"') # all batch elements hold same result
-	if os.path.isfile(FilePaths.fnOutPath):
-		txt_file = open(FilePaths.fnOutPath, 'a')
-	else:
-		txt_file = open(FilePaths.fnOutPath, 'x')
-	
-	txt_file.write(recognized[0] + "\n")
-	txt_file.close()
+def infer(model, imgName):
+    "recognize text in image provided by file path"
+    print(imgName)
+    img = preprocess(cv2.imread(imgName, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+    batch = Batch(None, [img] * Model.batchSize) # fill all batch elements with same input image
+    recognized = model.inferBatch(batch) # recognize text
+
+    # Open a file to append all word results to. All the words in the same directory belong to the same sentence.
+    if os.path.isfile(FilePaths.fnOutPath + FilePaths.fnRecognizedTxt):
+        txt_file = open(FilePaths.fnOutPath + FilePaths.fnRecognizedTxt, 'a')
+    else:
+        txt_file = open(FilePaths.fnOutPath + FilePaths.fnRecognizedTxt, 'x')
+
+    txt_file.write(recognized[0] + "\n")
+    txt_file.close()
+    # all batch elements hold the same result
+    print('Recognized: ', '"' + recognized[0] + '"' + " => written to " + FilePaths.fnOutPath + FilePaths.fnRecognizedTxt)
 
 
 def main():
-	"main function"
-	# optional command line args
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--train", help="train the NN", action="store_true")
-	parser.add_argument("--validate", help="validate the NN", action="store_true")
-	parser.add_argument("--beamsearch", help="use beam search instead of best path decoding", action="store_true")
-	parser.add_argument("--wordbeamsearch", help="use word beam search instead of best path decoding", action="store_true")
-	parser.add_argument('-fi', help="Path (from ../../02_deslanted/ on) and filename (without extension) to decode.")
-	parser.add_argument('-fo', help="Text filename and path (from ../../03_out/ on) to write the recognized words to.")
-	args = parser.parse_args()
+    "main function"
+    print("")
+    # optional command line args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", help="train the NN", action="store_true")
+    parser.add_argument("--validate", help="validate the NN", action="store_true")
+    parser.add_argument("--beamsearch", help="use beam search instead of best path decoding", action="store_true")
+    parser.add_argument("--wordbeamsearch", help="use word beam search instead of best path decoding", action="store_true")
+    parser.add_argument('-fi', help="Path (from ../../02_segmented/ on) and filename (without extension) to decode.")
+    parser.add_argument('-fo', help="Text filename and path (from ../../03_out/ on) to write the recognized words to.")
+    args = parser.parse_args()
 
-	decoderType = DecoderType.BestPath
-	if args.beamsearch:
-		decoderType = DecoderType.BeamSearch
-	elif args.wordbeamsearch:
-		decoderType = DecoderType.WordBeamSearch
-		
-	if args.fi:
-		inFileName = args.fi + ".png"
-		FilePaths.fnInPath = FilePaths.fnInPath + inFileName
-		print(FilePaths.fnInPath)
-	if args.fo:
-		outFileName = args.fo + ".png.txt"
-		FilePaths.fnOutPath = FilePaths.fnOutPath + outFileName
-		print(FilePaths.fnOutPath)
+    decoderType = DecoderType.BestPath
+    if args.beamsearch:
+        decoderType = DecoderType.BeamSearch
+    elif args.wordbeamsearch:
+        decoderType = DecoderType.WordBeamSearch
 
-	# train or validate on IAM dataset	
-	if args.train or args.validate:
-		# load training data, create TF model
-		loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
+    if args.fi:
+        FilePaths.fnInPath = FilePaths.fnInPath + args.fi
+    if args.fo:
+        FilePaths.fnOutPath = FilePaths.fnOutPath + args.fo
 
-		# save characters of model for inference mode
-		open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
-		
-		# save words contained in dataset into file
-		open(FilePaths.fnCorpus, 'w').write(str(' ').join(loader.trainWords + loader.validationWords))
+    if not os.path.exists(FilePaths.fnInPath):
+        print("No such path: " + FilePaths.fnInPath)
+        return(1)
 
-		# execute training or validation
-		if args.train:
-			model = Model(loader.charList, decoderType)
-			train(model, loader)
-		elif args.validate:
-			model = Model(loader.charList, decoderType, mustRestore=True)
-			validate(model, loader)
+    # train or validate on IAM dataset
+    if args.train or args.validate:
+        # load training data, create TF model
+        loader = DataLoader(FilePaths.fnTrain, Model.batchSize, Model.imgSize, Model.maxTextLen)
 
-	# infer text on test image
-	else:
-		print(open(FilePaths.fnAccuracy).read())
-		model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True)
-		infer(model, FilePaths.fnInPath)
+        # save characters of model for inference mode
+        open(FilePaths.fnCharList, 'w').write(str().join(loader.charList))
+
+        # save words contained in dataset into file
+        open(FilePaths.fnCorpus, 'w').write(str(' ').join(loader.trainWords + loader.validationWords))
+
+        # execute training or validation
+        if args.train:
+            model = Model(loader.charList, decoderType)
+            train(model, loader)
+        elif args.validate:
+            model = Model(loader.charList, decoderType, mustRestore=True)
+            validate(model, loader)
+
+    # infer text on test image
+    else:
+
+        if not os.path.exists(FilePaths.fnOutPath):
+            print("Created directory " + FilePaths.fnOutPath)
+            os.mkdir(FilePaths.fnOutPath)
+
+        imgFiles = sorted(os.listdir(FilePaths.fnInPath))
+        print("Files in directory: " + FilePaths.fnInPath + ": " + " ".join(imgFiles) + "\n")
+
+        print(open(FilePaths.fnAccuracy).read())
+        model = Model(open(FilePaths.fnCharList).read(), decoderType, mustRestore=True)
+
+        for (i,f) in enumerate(imgFiles):
+            if f == 'summary.png':
+                print("Dismissing " + '%s'%f)
+                continue
+
+            infer(model, FilePaths.fnInPath + '%s'%f)
+
+        print("")
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
